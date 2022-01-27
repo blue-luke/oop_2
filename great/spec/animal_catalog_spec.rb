8961470ca1b15dc7d2d6ec8724173eeede4cfd94
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-01-27 16:11:56 +0000

    Tests worked because 1 - cow tests were isolated and 2 - animal catalog tests hadn't been updated to reference cow

--- great/spec/animal_catalog_spec.rb
+++ great/spec/animal_catalog_spec.rb
@@ -8,6 +8,7 @@ describe AnimalCatalog do
       expect(catalog.sound(:dog)).to eq "woof"
       expect(catalog.sound(:cat)).to eq "meow"
       expect(catalog.sound(:bird)).to eq "chirp"
+      expect(catalog.sound(:cow)).to eq "moo"
     end
 
     it 'raises an error given an unrecognised animal' do
@@ -20,6 +21,7 @@ describe AnimalCatalog do
       expect(catalog.number_of_legs(:dog)).to eq 4
       expect(catalog.number_of_legs(:cat)).to eq 4
       expect(catalog.number_of_legs(:bird)).to eq 2
+      expect(catalog.number_of_legs(:cow)).to eq 4
     end
 
     it 'raises an error given an unrecognised animal' do
@@ -32,6 +34,7 @@ describe AnimalCatalog do
       expect(catalog.number_of_wings(:dog)).to eq 0
       expect(catalog.number_of_wings(:cat)).to eq 0
       expect(catalog.number_of_wings(:bird)).to eq 2
+      expect(catalog.number_of_wings(:cow)).to eq 0
     end
 
     it 'raises an error given an unrecognised animal' do
